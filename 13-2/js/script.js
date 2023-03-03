4635bafba46eaf8db18000c84be5cb1a23a618a1
krasapchik <kutman_mamytov359@gmail.com>
2023-03-04 00:35:31 +0600

    first commit

--- 13-2/js/script.js
+++ 13-2/js/script.js
@@ -1,46 +1,11 @@
 const projectA = document.getElementById('border1')
-const projectB = document.getElementById('border2')
-const projectC = document.getElementById('border3')
-const projectD = document.getElementById('border4')
-const projectE = document.getElementById('border5')
-const projectF = document.getElementById('border6')
-const projectG = document.getElementById('border7')
-const projectH = document.getElementById('border8')
-const projectI = document.getElementById('border9')
-const projectJ = document.getElementById('border10')
-const projectK = document.getElementById('border11')
-const projectL = document.getElementById('border12')
-
 
 const texts = async () =>{
     const object = await fetch('https://jsonplaceholder.typicode.com/posts')
     const jsobject = await object.json()
-    const result1 = jsobject[8]
-    const result2 = jsobject[9]
-    const result3 = jsobject[10]
-    const result4 = jsobject[11]
-    const result5 = jsobject[12]
-    const result6 = jsobject[13]
-    const result7 = jsobject[14]
-    const result8 = jsobject[15]
-    const result9 = jsobject[16]
-    const result10 = jsobject[17]
-    const result11 = jsobject[18]
-    const result12 = jsobject[19]
- 
-    console.log(jsobject);
-    projectA.innerHTML = `<p>User id: 1</p><p>Id:9</p>${result1.title}<br>${result1.body}`
-    projectB.innerHTML = `<p>User id: 1</p><p>Id:10</p>${result2.title}<br>${result2.body}`
-    projectC.innerHTML = `<p>User id: 2</p><p>Id:11</p>${result3.title}<br>${result3.body}`
-    projectD.innerHTML = `<p>User id: 2</p><p>Id:12</p>${result4.title}<br>${result4.body}`
-    projectE.innerHTML = `<p>User id: 2</p><p>Id:13</p>${result5.title}<br>${result6.body}`
-    projectF.innerHTML = `<p>User id: 2</p><p>Id:14</p>${result6.title}<br>${result6.body}`
-    projectG.innerHTML = `<p>User id: 2</p><p>Id:15</p>${result7.title}<br>${result7.body}`
-    projectH.innerHTML = `<p>User id: 2</p><p>Id:16</p>${result8.title}<br>${result8.body}`
-    projectI.innerHTML = `<p>User id: 2</p><p>Id:17</p>${result9.title}<br>${result9.body}`
-    projectJ.innerHTML = `<p>User id: 2</p><p>Id:18</p>${result10.title}<br>${result10.body}`
-    projectK.innerHTML = `<p>User id: 2</p><p>Id:19</p>${result11.title}<br>${result11.body}`
-    projectL.innerHTML = `<p>User id: 2</p><p>Id:20</p>${result12.title}<br>${result12.body}`
+  const data = jsobject.map((el) =>{
+projectA.innerHTML += `<div class ="bo"><p>User id:${el.userId}</p><p>Id:${el.id} </p>${el.title}${el.body}</div>`
+  })
 
 }
 
--- js/script.js
+++ js/script.js
@@ -1,45 +1,16 @@
 const projectA = document.getElementById('list')
-const projectB = document.getElementById('list2')
-const projectC = document.getElementById('list3')
-const projectD = document.getElementById('list4')
-const projectE = document.getElementById('list5')
-const projectF = document.getElementById('list6')
-
 
 const getData = async () => {
     const pesponse = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await pesponse.json()
-   const result  = data[0]
-      const result2 = data[1]
-        const result3 = data[2]
-           const result4  = data[3]
-                 const result5 = data[4]
-              const result6 = data[5]
-           const keys = Object.keys(result)
-         const poro = keys[1]
-      const poro2 = keys[2]
-
-  console.log(result);
-
-            projectA.innerHTML = `<h2>1</h2> <b>${poro}</b>: ${result.name} <br> <b>phone: </b>${result.phone}<b><br>${poro2}:</b>${result.username}<br>
-                                            <b>website:</b>${result.website}<br><b>email: </b>${result.email}`
-
-            projectB.innerHTML = `<h2>2</h2> <b>${poro}</b>: ${result2.name} <br><b>phone: </b>${result2.phone}<br><b>
-                                               ${poro2}:</b>${result2.username}<br><b>website:</b>${result2.website}<br><b>email:</b>${result2.email}`
-
-            projectC.innerHTML = ` <h2>3</h2><b>${poro}</b>: ${result3.name} <br><b>phone: </b>${result3.phone}<br><b>${poro2}:</b>${result3.username}<br>
-                                      <b>website:</b>${result3.website}<br><b>email:</b>${result3.email} `
-
-            projectD.innerHTML = `<h2>4</h2> <b>${poro}</b>: ${result4.name} <br><b>phone: </b>${result4.phone}<br><b>${poro2}:</b>${result4.username}<br>
-                                       <b>website:</b>${result4.website}<br><b>email:</b>${result4.email} `
-
-             projectE.innerHTML = `<h2>5</h2> <b>${poro}</b>: ${result5.name} <br><b>phone: </b>${result5.phone}<br><b>${poro2}:</b>${result5.username}<br>
-                                             <b>website:</b>${result5.website}<br><b>email:</b>${result5.email} `
-
-            projectF.innerHTML = `<h2>6</h2> <b>${poro}</b>: ${result6.name} <br><b>phone: </b>${result6.phone}<br><b>${poro2}:</b>${result6.username}<br>
-                                              <b>website:</b>${result6.website}<br><b>email:</b>${result6.email} `
+   const keys = data.map((el) =>{
+      projectA.innerHTML +=`<div class ="heder"><h2>${el.id}</h2><b>name: </b>${el.name}<br>
+      <b>phone:</b>${el.phone}<br><b>username:</b>${el.username}<br><b>website:</b>${el.website}<br><b>email:</b>${el.email}</div>`
+   })
 
 
 }
 getData()
+
+
  
